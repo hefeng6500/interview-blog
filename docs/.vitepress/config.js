c0453635313d6d1da134cc2a5985752e294f1d3a
hefeng6500 <2443992009@qq.com>
2021-11-07 20:51:04 +0800

    217. 存在重复元素

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,19 +1,18 @@
 module.exports = {
-  base:'/interview-blog/',
-  lang: 'zh-CN',
-  title: '前端学习记录',
-  description: 'hefeng6500 的前端学习笔记，记录学习历程，分享学习总结！',
+  base: "/interview-blog/",
+  lang: "zh-CN",
+  title: "前端学习记录",
+  description: "hefeng6500 的前端学习笔记，记录学习历程，分享学习总结！",
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
-      { text: 'javascript', link: '/javascript/' },
-      { text: '算法', link: '/algorithm/' },
-      
+      { text: "首页", link: "/" },
+      { text: "javascript", link: "/javascript/" },
+      { text: "数据结构与算法", link: "/algorithm/" },
     ],
     sidebar: {
-      "javascript": [
+      javascript: [
         {
-          text: '深入javascript',
+          text: "深入javascript",
           children: [
             { text: "JavaScript深入之执行上下文", link: "/javascript/1" },
             { text: "JavaScript深入之作用域链", link: "/javascript/2" },
@@ -21,32 +20,39 @@ module.exports = {
             { text: "JavaScript深入之call和apply", link: "/javascript/4" },
             { text: "JavaScript深入之bind", link: "/javascript/5" },
             { text: "JavaScript深入之new", link: "/javascript/6" },
-            { text: "JavaScript深入之类数组对象arguments", link: "/javascript/7" },
+            {
+              text: "JavaScript深入之类数组对象arguments",
+              link: "/javascript/7",
+            },
             { text: "JavaScript深入之循环和iterator", link: "/javascript/8" },
             { text: "JavaScript深入之原型和原型链", link: "/javascript/9" },
             { text: "JavaScript深入之继承", link: "/javascript/10" },
-          ]
-        }
+          ],
+        },
       ],
-      "algorithm": [
+      algorithm: [
         {
-          text: '数据结构',
+          text: "数据结构",
           children: [
             { text: "栈", link: "/algorithm/1" },
             { text: "队列", link: "/algorithm/2" },
-          ]
+          ],
         },
         {
-          text: '算法',
+          text: "算法",
           children: [
-          ]
+            {
+              text: "数组",
+              children: [
+                {
+                  text: "217.存在重复元素 2021.11.07",
+                  link: "/algorithm/array/217.存在重复元素",
+                },
+              ],
+            },
+          ],
         },
-        {
-          text: '刷题',
-          children: [
-          ]
-        }
-      ]
+      ],
     },
   },
-}
+};
