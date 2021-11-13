faff02a76a559b42c8305b98c1273f6e4a2fe6af
hefeng6500 <2443992009@qq.com>
2021-11-13 20:27:46 +0800

    fix

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import Comment from '../../components/comment/index.vue'
+
+const { Layout } = DefaultTheme
+
+</script>
+
+<template>
+  <Layout>
+    <template #page-bottom>
+      <Comment />
+    </template>
+  </Layout>
+</template>
