b10a2e707b4e2423c49585f77d22c65f9795d7a7
兰亭古墨 <2443992009@qq.com>
2021-11-16 21:42:45 +0800

    fix comment

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -1,18 +1,29 @@
 <script setup>
+import { ref, watch, nextTick } from "vue";
 import { useRouter } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import Comment from "../../components/comment/index.vue";
 import md5 from "md5";
 
-const { Layout } = DefaultTheme;
 const { route } = useRouter();
 const id = md5(route.path);
+const { Layout } = DefaultTheme;
+
+let enableComment = ref(false);
+
+watch(route, async (newValue) => {
+  enableComment.value = false;
+
+  await nextTick();
+
+  enableComment.value = true;
+});
 </script>
 
 <template>
   <Layout>
     <template #page-bottom>
-      <Comment :id="id" />
+      <Comment v-if="enableComment" />
     </template>
   </Layout>
 </template>
--- docs/components/comment/index.vue
+++ docs/components/comment/index.vue
@@ -1,34 +1,32 @@
 <template>
   <div id="gitalk-container"></div>
+  <span v-show="false">{{ id }}</span>
 </template>
 
-<script>
-import Gitalk from 'gitalk'
-import 'gitalk/dist/gitalk.css'
+<script setup>
+import { computed, nextTick } from "vue";
+import { useRouter } from "vitepress";
+import md5 from "md5";
 
+import Gitalk from "gitalk";
+import "gitalk/dist/gitalk.css";
 
-export default {
-  props: {
-    id: {
-      type: String
-    }
-  },
-  mounted() {
-    const gitalk = new Gitalk({
-      clientID: '8760b254461e3650fc51',
-      clientSecret: 'ca8c12795123d4605d8d98a2753f2b5d78049832',
-      repo: 'interview-blog',
-      owner: 'hefeng6500',
-      admin: ['hefeng6500'],
-      id: this.id,
-      distractionFreeMode: false
-    })
+const id = computed(async () => {
+  const { route } = useRouter();
+  const id = md5(route.path);
 
-    console.log(gitalk);
+  await nextTick();
 
-    gitalk.render('gitalk-container')
+  const gitalk = new Gitalk({
+    clientID: "8760b254461e3650fc51",
+    clientSecret: "ca8c12795123d4605d8d98a2753f2b5d78049832",
+    repo: "interview-blog",
+    owner: "hefeng6500",
+    admin: ["hefeng6500"],
+    id,
+    distractionFreeMode: false,
+  });
 
-  }
-}
+  gitalk.render("gitalk-container");
+});
 </script>
- 
