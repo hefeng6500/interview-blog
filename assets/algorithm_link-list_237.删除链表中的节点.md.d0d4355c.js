fc3661c57b72006e1a2a2882cda69a4827ffb2be
hefeng6500 <2443992009@qq.com>
2021-12-03 14:14:54 +0000

    Deploying to gh-pages from @ hefeng6500/interview-blog@64cc064e22b58c222cf6bb004bd3eee8b8b8033b 🚀

---
+++ assets/algorithm_link-list_237.删除链表中的节点.md.d0d4355c.js
@@ -0,0 +1,28 @@
+import{_ as n,c as a,o as s,e}from"./app.b7db8792.js";const k='{"title":"237. \u5220\u9664\u94FE\u8868\u4E2D\u7684\u8282\u70B9","description":"","frontmatter":{},"headers":[{"level":3,"title":"\u89E3\u6CD5","slug":"\u89E3\u6CD5"}],"relativePath":"algorithm/link-list/237.\u5220\u9664\u94FE\u8868\u4E2D\u7684\u8282\u70B9.md","lastUpdated":1638540874781}',t={},o=e(`<h1 id="_237-\u5220\u9664\u94FE\u8868\u4E2D\u7684\u8282\u70B9" tabindex="-1">237. \u5220\u9664\u94FE\u8868\u4E2D\u7684\u8282\u70B9 <a class="header-anchor" href="#_237-\u5220\u9664\u94FE\u8868\u4E2D\u7684\u8282\u70B9" aria-hidden="true">#</a></h1><p>\u8BF7\u7F16\u5199\u4E00\u4E2A\u51FD\u6570\uFF0C\u7528\u4E8E <strong>\u5220\u9664\u5355\u94FE\u8868\u4E2D\u67D0\u4E2A\u7279\u5B9A\u8282\u70B9</strong> \u3002\u5728\u8BBE\u8BA1\u51FD\u6570\u65F6\u9700\u8981\u6CE8\u610F\uFF0C\u4F60\u65E0\u6CD5\u8BBF\u95EE\u94FE\u8868\u7684\u5934\u8282\u70B9 \xA0head \uFF0C\u53EA\u80FD\u76F4\u63A5\u8BBF\u95EE <strong>\u8981\u88AB\u5220\u9664\u7684\u8282\u70B9</strong> \u3002</p><p>\u9898\u76EE\u6570\u636E\u4FDD\u8BC1\u9700\u8981\u5220\u9664\u7684\u8282\u70B9 <strong>\u4E0D\u662F\u672B\u5C3E\u8282\u70B9</strong> \u3002</p><p><strong>\u793A\u4F8B 1\uFF1A</strong></p><div class="language-"><pre><code>\u8F93\u5165\uFF1Ahead = [4,5,1,9], node = 5
+\u8F93\u51FA\uFF1A[4,1,9]
+\u89E3\u91CA\uFF1A\u6307\u5B9A\u94FE\u8868\u4E2D\u503C\u4E3A\xA05\xA0\u7684\u7B2C\u4E8C\u4E2A\u8282\u70B9\uFF0C\u90A3\u4E48\u5728\u8C03\u7528\u4E86\u4F60\u7684\u51FD\u6570\u4E4B\u540E\uFF0C\u8BE5\u94FE\u8868\u5E94\u53D8\u4E3A 4 -&gt; 1 -&gt; 9
+</code></pre></div><p><strong>\u793A\u4F8B 2\uFF1A</strong></p><div class="language-"><pre><code>\u8F93\u5165\uFF1Ahead = [4,5,1,9], node = 1
+\u8F93\u51FA\uFF1A[4,5,9]
+\u89E3\u91CA\uFF1A\u6307\u5B9A\u94FE\u8868\u4E2D\u503C\u4E3A\xA01\xA0\u7684\u7B2C\u4E09\u4E2A\u8282\u70B9\uFF0C\u90A3\u4E48\u5728\u8C03\u7528\u4E86\u4F60\u7684\u51FD\u6570\u4E4B\u540E\uFF0C\u8BE5\u94FE\u8868\u5E94\u53D8\u4E3A 4 -&gt; 5 -&gt; 9
+</code></pre></div><p><strong>\u793A\u4F8B 3\uFF1A</strong></p><div class="language-"><pre><code>\u8F93\u5165\uFF1Ahead = [1,2,3,4], node = 3
+\u8F93\u51FA\uFF1A[1,2,4]
+</code></pre></div><p><strong>\u793A\u4F8B 4\uFF1A</strong></p><div class="language-"><pre><code>\u8F93\u5165\uFF1Ahead = [0,1], node = 0
+\u8F93\u51FA\uFF1A[1]
+</code></pre></div><p><strong>\u793A\u4F8B 5\uFF1A</strong></p><div class="language-"><pre><code>\u8F93\u5165\uFF1Ahead = [-3,5,-99], node = -3
+\u8F93\u51FA\uFF1A[5,-99]
+</code></pre></div><p>\u63D0\u793A\uFF1A</p><p>\u94FE\u8868\u4E2D\u8282\u70B9\u7684\u6570\u76EE\u8303\u56F4\u662F [2, 1000] -1000 &lt;= Node.val &lt;= 1000 \u94FE\u8868\u4E2D\u6BCF\u4E2A\u8282\u70B9\u7684\u503C\u90FD\u662F\u552F\u4E00\u7684 \u9700\u8981\u5220\u9664\u7684\u8282\u70B9 node \u662F \u94FE\u8868\u4E2D\u7684\u4E00\u4E2A\u6709\u6548\u8282\u70B9 \uFF0C\u4E14 \u4E0D\u662F\u672B\u5C3E\u8282\u70B9</p><h3 id="\u89E3\u6CD5" tabindex="-1">\u89E3\u6CD5 <a class="header-anchor" href="#\u89E3\u6CD5" aria-hidden="true">#</a></h3><p>\u8FD9\u4E2A\u5C31\u5F88\u7B80\u5355\u4E86\uFF0C\u524D\u63D0\u662F\u719F\u6089\u94FE\u8868\u8FD9\u4E2A\u6570\u636E\u7ED3\u6784</p><div class="language-js"><pre><code><span class="token comment">/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */</span>
+<span class="token comment">/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */</span>
+<span class="token keyword">var</span> <span class="token function-variable function">deleteNode</span> <span class="token operator">=</span> <span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token parameter">node</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  node<span class="token punctuation">.</span>val <span class="token operator">=</span> node<span class="token punctuation">.</span>next<span class="token punctuation">.</span>val<span class="token punctuation">;</span>
+  node<span class="token punctuation">.</span>next <span class="token operator">=</span> node<span class="token punctuation">.</span>next<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre></div>`,18),p=[o];function c(d,r,i,l,u,_){return s(),a("div",null,p)}var h=n(t,[["render",c]]);export{k as __pageData,h as default};
---
+++ assets/algorithm_string_125.验证回文串.md.3b7c8148.lean.js
@@ -0,0 +1,68 @@
+import{_ as n,c as s,o as a,e as t}from"./app.b7db8792.js";const f='{"title":"125.\u9A8C\u8BC1\u56DE\u6587\u4E32","description":"","frontmatter":{},"headers":[{"level":3,"title":"\u89E3\u6CD5","slug":"\u89E3\u6CD5"}],"relativePath":"algorithm/string/125.\u9A8C\u8BC1\u56DE\u6587\u4E32.md","lastUpdated":1638540874781}',p={},o=t(`__VP_STATIC_START__<h1 id="_125-\u9A8C\u8BC1\u56DE\u6587\u4E32" tabindex="-1">125.\u9A8C\u8BC1\u56DE\u6587\u4E32 <a class="header-anchor" href="#_125-\u9A8C\u8BC1\u56DE\u6587\u4E32" aria-hidden="true">#</a></h1><p>\u7ED9\u5B9A\u4E00\u4E2A\u5B57\u7B26\u4E32\uFF0C\u9A8C\u8BC1\u5B83\u662F\u5426\u662F\u56DE\u6587\u4E32\uFF0C\u53EA\u8003\u8651\u5B57\u6BCD\u548C\u6570\u5B57\u5B57\u7B26\uFF0C\u53EF\u4EE5\u5FFD\u7565\u5B57\u6BCD\u7684\u5927\u5C0F\u5199\u3002</p><p>\u8BF4\u660E\uFF1A\u672C\u9898\u4E2D\uFF0C\u6211\u4EEC\u5C06\u7A7A\u5B57\u7B26\u4E32\u5B9A\u4E49\u4E3A\u6709\u6548\u7684\u56DE\u6587\u4E32\u3002</p><p><strong>\u793A\u4F8B 1:</strong></p><div class="language-"><pre><code>\u8F93\u5165: &quot;A man, a plan, a canal: Panama&quot;
+\u8F93\u51FA: true
+\u89E3\u91CA\uFF1A&quot;amanaplanacanalpanama&quot; \u662F\u56DE\u6587\u4E32
+</code></pre></div><p><strong>\u793A\u4F8B 2:</strong></p><div class="language-"><pre><code>\u8F93\u5165: &quot;race a car&quot;
+\u8F93\u51FA: false
+\u89E3\u91CA\uFF1A&quot;raceacar&quot; \u4E0D\u662F\u56DE\u6587\u4E32
+</code></pre></div><p>\u63D0\u793A\uFF1A</p><p>1 &lt;= s.length &lt;= 2 * 105 \u5B57\u7B26\u4E32 s \u7531 ASCII \u5B57\u7B26\u7EC4\u6210</p><h3 id="\u89E3\u6CD5" tabindex="-1">\u89E3\u6CD5 <a class="header-anchor" href="#\u89E3\u6CD5" aria-hidden="true">#</a></h3><p><strong>1\u3001\u6B63\u5219\u8868\u8FBE\u5F0F + \u5B57\u7B26\u4E32\u53CD\u8F6C</strong></p><div class="language-js"><pre><code><span class="token comment">/**
+ * @param {string} s
+ * @return {boolean}
+ */</span>
+<span class="token keyword">var</span> <span class="token function-variable function">isPalindrome</span> <span class="token operator">=</span> <span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token parameter">s</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  <span class="token keyword">const</span> str <span class="token operator">=</span> s<span class="token punctuation">.</span><span class="token function">replaceAll</span><span class="token punctuation">(</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">[^A-Za-z0-9]</span><span class="token regex-delimiter">/</span><span class="token regex-flags">g</span></span><span class="token punctuation">,</span> <span class="token string">&quot;&quot;</span><span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">toLocaleLowerCase</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+  <span class="token keyword">const</span> cache <span class="token operator">=</span> str<span class="token punctuation">.</span><span class="token function">split</span><span class="token punctuation">(</span><span class="token string">&quot;&quot;</span><span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">reverse</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">join</span><span class="token punctuation">(</span><span class="token string">&quot;&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+  <span class="token keyword">return</span> str <span class="token operator">===</span> cache<span class="token punctuation">;</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre></div><p><strong>2\u3001\u53CC\u6307\u9488</strong></p><ul><li>\u4E00\u4E2A\u5DE6\u6307\u9488\uFF0C\u4E00\u4E2A\u53F3\u6307\u9488\uFF0C</li><li>\u9047\u5230\u975E\u5B57\u6BCD\u548C\u6570\u5B57\u5C31\u8DF3\u8FC7\uFF0C\u9047\u5230\u5B57\u6BCD\u5C31\u8FDB\u884C\u6BD4\u5BF9</li><li>\u5982\u679C\u6700\u540E\u6CA1\u6709\u4E0D\u60F3\u7B49\u7684\u4EE3\u8868\u662F\u56DE\u6587\u5B57\u7B26\u4E32</li></ul><div class="language-js"><pre><code><span class="token comment">/**
+ * @param {string} s
+ * @return {boolean}
+ */</span>
+<span class="token keyword">var</span> <span class="token function-variable function">isPalindrome</span> <span class="token operator">=</span> <span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token parameter">s</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  <span class="token keyword">let</span> left <span class="token operator">=</span> <span class="token number">0</span><span class="token punctuation">;</span>
+  <span class="token keyword">let</span> right <span class="token operator">=</span> s<span class="token punctuation">.</span>length <span class="token operator">-</span> <span class="token number">1</span><span class="token punctuation">;</span>
+  <span class="token keyword">const</span> reg <span class="token operator">=</span> <span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">[a-zA-Z0-9]</span><span class="token regex-delimiter">/</span></span><span class="token punctuation">;</span>
+
+  <span class="token keyword">while</span> <span class="token punctuation">(</span>left <span class="token operator">&lt;</span> right<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span><span class="token operator">!</span>reg<span class="token punctuation">.</span><span class="token function">test</span><span class="token punctuation">(</span>s<span class="token punctuation">[</span>left<span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      left<span class="token operator">++</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token keyword">if</span> <span class="token punctuation">(</span><span class="token operator">!</span>reg<span class="token punctuation">.</span><span class="token function">test</span><span class="token punctuation">(</span>s<span class="token punctuation">[</span>right<span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      right<span class="token operator">--</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token punctuation">{</span>
+      <span class="token keyword">if</span> <span class="token punctuation">(</span>s<span class="token punctuation">[</span>left<span class="token punctuation">]</span><span class="token punctuation">.</span><span class="token function">toLocaleLowerCase</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">!==</span> s<span class="token punctuation">[</span>right<span class="token punctuation">]</span><span class="token punctuation">.</span><span class="token function">toLocaleLowerCase</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">return</span> <span class="token boolean">false</span><span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+      left<span class="token operator">++</span><span class="token punctuation">;</span>
+      right<span class="token operator">--</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+
+  <span class="token keyword">return</span> <span class="token boolean">true</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre></div><p><strong>3\u3001\u9012\u5F52\u5B9E\u73B0</strong></p><p>\u73A9\u51FA\u82B1\u6837\u7684\u65B9\u5F0F</p><div class="language-js"><pre><code><span class="token comment">/**
+ * @param {string} s
+ * @return {boolean}
+ */</span>
+<span class="token keyword">var</span> <span class="token function-variable function">isPalindrome</span> <span class="token operator">=</span> <span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token parameter">s</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  <span class="token keyword">const</span> reg <span class="token operator">=</span> <span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">[a-zA-Z0-9]</span><span class="token regex-delimiter">/</span></span><span class="token punctuation">;</span>
+
+  <span class="token keyword">const</span> <span class="token function-variable function">isPalindromeHelper</span> <span class="token operator">=</span> <span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token parameter">s<span class="token punctuation">,</span> left<span class="token punctuation">,</span> right</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>left <span class="token operator">&gt;=</span> right<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">return</span> <span class="token boolean">true</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">while</span> <span class="token punctuation">(</span>left <span class="token operator">&lt;</span> right <span class="token operator">&amp;&amp;</span> <span class="token operator">!</span>reg<span class="token punctuation">.</span><span class="token function">test</span><span class="token punctuation">(</span>s<span class="token punctuation">[</span>left<span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      left<span class="token operator">++</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">while</span> <span class="token punctuation">(</span>left <span class="token operator">&lt;</span> right <span class="token operator">&amp;&amp;</span> <span class="token operator">!</span>reg<span class="token punctuation">.</span><span class="token function">test</span><span class="token punctuation">(</span>s<span class="token punctuation">[</span>right<span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      right<span class="token operator">--</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token keyword">return</span> <span class="token punctuation">(</span>
+      s<span class="token punctuation">[</span>left<span class="token punctuation">]</span><span class="token punctuation">.</span><span class="token function">toLocaleLowerCase</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">===</span> s<span class="token punctuation">[</span>right<span class="token punctuation">]</span><span class="token punctuation">.</span><span class="token function">toLocaleLowerCase</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">&amp;&amp;</span>
+      <span class="token function">isPalindromeHelper</span><span class="token punctuation">(</span>s<span class="token punctuation">,</span> <span class="token operator">++</span>left<span class="token punctuation">,</span> <span class="token operator">--</span>right<span class="token punctuation">)</span>
+    <span class="token punctuation">)</span><span class="token punctuation">;</span>
+  <span class="token punctuation">}</span><span class="token punctuation">;</span>
+
+  <span class="token keyword">return</span> <span class="token function">isPalindromeHelper</span><span class="token punctuation">(</span>s<span class="token punctuation">,</span> <span class="token number">0</span><span class="token punctuation">,</span> s<span class="token punctuation">.</span>length <span class="token operator">-</span> <span class="token number">1</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre></div>__VP_STATIC_END__`,18),e=[o];function c(l,u,k,r,i,d){return a(),s("div",null,e)}var m=n(p,[["render",c]]);export{f as __pageData,m as default};
