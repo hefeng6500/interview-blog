00cc69e772a37a48282b1f624e7766c2bd6e877e
hefeng6500 <2443992009@qq.com>
2021-12-19 13:57:12 +0800

    google analysis

--- docs/.vitepress/utils/index.js
+++ docs/.vitepress/utils/index.js
@@ -8,6 +8,26 @@ const useBaiduTongji = () => {
   })();
 };
 
+const useGoogleTongji = () => {
+  // <!-- Global site tag (gtag.js) - Google Analytics -->
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-G913KVNC8X";
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "G-G913KVNC8X");
+
+  const s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(script, s);
+};
+
 export const generateTongji = () => {
   useBaiduTongji();
+  useGoogleTongji();
 };
