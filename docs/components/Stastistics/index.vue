b4eaa29cf1885d5ffe0f92e6cd4526ceae1d6924
hefeng6500 <2443992009@qq.com>
2021-12-19 13:13:00 +0800

    modify

--- docs/components/Stastistics/index.vue
+++ docs/components/Stastistics/index.vue
@@ -1,9 +1,13 @@
-<script>
-var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?1de472432e76a93aa5f97f550145dfb4";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
+<script setup>
+import { onBeforeMount } from "vue";
+
+onBeforeMount(() => {
+  var _hmt = _hmt || [];
+  (function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?1de472432e76a93aa5f97f550145dfb4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+});
 </script>
